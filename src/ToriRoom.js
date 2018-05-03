7c8691673bd56b050974dfd43901181ae4f629bd
Marcella (Cindy) Prasetio <m.cindy.prasetio@gmail.com>
2018-05-03 02:18:49 -0700

    User can place accessories

--- src/ToriRoom.js
+++ src/ToriRoom.js
@@ -1,6 +1,38 @@
 import React, { Component, PropTypes } from 'react'
 
-import GridLayout from 'react-grid-layout';
+import { withStyles } from 'material-ui/styles';
+import GridList, { GridListTile } from 'material-ui/GridList';
+
+// Import Image
+import Floor from '../public/img/floor.png';
+
+const LIM = 5;
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 400,
+    height: 400,
+  },
+  gridTile: {
+    backgroundImage: `url(${Floor})`,
+    backgroundSize: 80,
+  },
+  gridEmpty: {
+    backgroundColor: theme.palette.primary.main,
+    width: `90%`,
+    height: `90%`,
+    opacity: 0.3,
+    margin: `5%`
+  }
+});
+
 
 class ToriRoom extends Component {
 
@@ -8,22 +40,38 @@ class ToriRoom extends Component {
     super(props)
 
     this.state = {
-      layout: [
-        {i: 'tori', x: 2, y: 2, w: 1, h: 1, static: false},
-        {i: 'door', x: 2, y: 4, w: 1, h: 1, static: true},
-      ],
-      cells: [
-        (<div key="tori" className="cell tori">
-          <img src='mockimg/tori-sample.png'/>
-        </div>),
-        (<div key="door" className="cell">
-        </div>),
-      ],
+      cells: [],
+      isSelecting: false,
+      layout: [],
+      layoutIndicator: []
     }
   }
 
   componentDidMount() {
     // TODO: get info about the layout here.
+
+    // Construct the initial layout.
+    let layout = [
+      {
+          key: 'tori',
+          img: 'mockimg/tori-sample.png',
+          col: 2,
+          row: 2,
+      },
+    ];
+
+    let layoutIndicator = [0, 0, 0, 0, 0,
+                           0, 0, 0, 0, 0,
+                           0, 0, 1, 0, 0,
+                           0, 0, 0, 0, 0,
+                           0, 0, 0, 0, 0]
+
+    // TODO: modify the layoutIndicator based on the retrieved layout.
+
+    this.setState({
+      layout: layout,
+      layoutIndicator: layoutIndicator
+    });
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -35,44 +83,100 @@ class ToriRoom extends Component {
     }
   }
 
+  constructCells() {
+    let idx = 0;
+    // First, construct all grid with Floor.
+    let cells = this.state.layoutIndicator.map((val) => {
+      let x = idx % LIM;
+      let y = Math.floor(idx / LIM);
+      let c;
+      if (val === 0 && this.state.isSelecting) {
+        c = (
+          <GridListTile onClick={(e) => this.onTileClick(x, y, e)}
+                        key={`cell_${x}_${y}`}
+                        className={this.props.classes.gridTile}
+                        cols={1}>
+            <div className={this.props.classes.gridEmpty}></div>
+          </GridListTile>
+        )
+      } else {
+        c = (
+          <GridListTile key={`cell_${x}_${y}`}
+                        className={this.props.classes.gridTile}
+                        cols={1}>
+          </GridListTile>
+        )
+      }
+      idx += 1;
+      return c;
+    });
+
+    // Second, iterate through layout and reassign cells.
+    this.state.layout.forEach((content) => {
+      let x = content.col;
+      let y = content.row;
+      let img = content.img;
+      let key = content.key;
+
+      let c = (
+        <GridListTile key={key} className={this.props.classes.gridTile} cols={1}>
+          <img src={img} alt={key} />
+        </GridListTile>
+      );
+
+      cells[y*LIM + x] = c;
+    });
+
+    return cells;
+  }
+
   onItemSelected() {
+    this.setState({
+      isSelecting: true,
+    })
+  }
+
+  onTileClick(x, y, e) {
+    console.log('clicked', x, y)
+    // TODO: Check if valid size.
+
+    // Update cell.
     let key = this.props.acc.key;
     let space = this.props.acc.space;
     let img = this.props.acc.img;
-    let newCell = (
-      <div key={key} className="cell" preventCollision={true}>
-        <img src={img}/>
-      </div>
-    );
 
-    // TODO: Calculate where this object should be placed.
-    let newLayout = {i: key, x: 0, y: 0, w: 1, h: 1, static: false};
+    // TODO: update with size.
+    let l = {
+      key: key,
+      img: img,
+      col: x,
+      row: y,
+    };
 
-    // Update the state.
+    let layoutIndicator = this.state.layoutIndicator;
+    layoutIndicator[y*LIM + x] = 1;
+    let layout = this.state.layout;
+
     this.setState({
-      layout: this.state.layout.concat(newLayout),
-      cells: this.state.cells.concat(newCell),
-    });
-
-    console.log(this.state.layout.length)
+      layoutIndicator: layoutIndicator,
+      layout: layout.concat(l),
+      isSelecting: false,
+    })
   }
 
   render() {
-
+    let cells = this.constructCells();
     return (
-      <div className="tori-room">
-        <GridLayout className="layout"
-                    layout={this.state.layout}
-                    cols={5}
-                    rows={5}
-                    width={400}
-                    margin={[0, 0]}
-                    rowHeight={80}>
-          {this.state.cells}
-        </GridLayout>
+      <div className={this.props.classes.root}>
+        <GridList cellHeight={80}
+                  className={this.props.classes.gridList}
+                  cols={5}
+                  spacing={0}>
+          {cells}
+        </GridList>
       </div>
     );
   }
 }
 
-export default ToriRoom
+export default withStyles(styles)(ToriRoom)
